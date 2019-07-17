92f6902752223362dcf6fa678314769f442af669
znsc <53016754+znsc@users.noreply.github.com>
2019-07-17 20:08:48 +0200

    Add files via upload

---
+++ SRResNet.py
@@ -0,0 +1,102 @@
+import functools
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+
+def initialize_weights(net_l, scale=1):
+    if not isinstance(net_l, list):
+        net_l = [net_l]
+    for net in net_l:
+        for m in net.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+                m.weight.data *= scale  # for residual block
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+                m.weight.data *= scale
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias.data, 0.0)
+
+
+def make_layer(block, n_layers):
+    layers = []
+    for _ in range(n_layers):
+        layers.append(block())
+    return nn.Sequential(*layers)
+
+
+class ResidualBlock_noBN(nn.Module):
+    '''Residual block w/o BN
+    ---Conv-ReLU-Conv-+-
+     |________________|
+    '''
+
+    def __init__(self, nf=64):
+        super(ResidualBlock_noBN, self).__init__()
+        self.conv1 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.conv2 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+
+        # initialization
+        initialize_weights([self.conv1, self.conv2], 0.1)
+
+    def forward(self, x):
+        identity = x
+        out = F.relu(self.conv1(x), inplace=True)
+        out = self.conv2(out)
+        return identity + out
+
+
+class MSRResNet(nn.Module):
+    ''' modified SRResNet'''
+
+    def __init__(self, in_nc=3, out_nc=3, nf=64, nb=16, upscale=4):
+        super(MSRResNet, self).__init__()
+        self.upscale = upscale
+
+        self.conv_first = nn.Conv2d(in_nc, nf, 3, 1, 1, bias=True)
+        basic_block = functools.partial(ResidualBlock_noBN, nf=nf)
+        self.recon_trunk = make_layer(basic_block, nb)
+
+        # upsampling
+        if self.upscale == 2:
+            self.upconv1 = nn.Conv2d(nf, nf * 4, 3, 1, 1, bias=True)
+            self.pixel_shuffle = nn.PixelShuffle(2)
+        elif self.upscale == 3:
+            self.upconv1 = nn.Conv2d(nf, nf * 9, 3, 1, 1, bias=True)
+            self.pixel_shuffle = nn.PixelShuffle(3)
+        elif self.upscale == 4:
+            self.upconv1 = nn.Conv2d(nf, nf * 4, 3, 1, 1, bias=True)
+            self.upconv2 = nn.Conv2d(nf, nf * 4, 3, 1, 1, bias=True)
+            self.pixel_shuffle = nn.PixelShuffle(2)
+
+        self.HRconv = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.conv_last = nn.Conv2d(nf, out_nc, 3, 1, 1, bias=True)
+
+        # activation function
+        self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+
+        # initialization
+        initialize_weights([self.conv_first, self.upconv1, self.HRconv, self.conv_last], 0.1)
+        if self.upscale == 4:
+            initialize_weights(self.upconv2, 0.1)
+
+    def forward(self, x):
+        fea = self.lrelu(self.conv_first(x))
+        out = self.recon_trunk(fea)
+
+        if self.upscale == 4:
+            out = self.lrelu(self.pixel_shuffle(self.upconv1(out)))
+            out = self.lrelu(self.pixel_shuffle(self.upconv2(out)))
+        elif self.upscale == 3 or self.upscale == 2:
+            out = self.lrelu(self.pixel_shuffle(self.upconv1(out)))
+
+        out = self.conv_last(self.lrelu(self.HRconv(out)))
+        base = F.interpolate(x, scale_factor=self.upscale, mode='bilinear', align_corners=False)
+        out += base
+        return out
---
+++ test_demo.py
@@ -0,0 +1,99 @@
+import os.path
+import logging
+import time
+from collections import OrderedDict
+import torch
+
+from utils import utils_logger
+from utils import utils_image as util
+from SRResNet import MSRResNet
+
+
+def main():
+
+    utils_logger.logger_info('AIM-track', log_path='AIM-track.log')
+    logger = logging.getLogger('AIM-track')
+
+    # --------------------------------
+    # basic settings
+    # --------------------------------
+    testsets = 'DIV2K'
+    testset_L = 'DIV2K_valid_LR_bicubic'
+
+    torch.cuda.current_device()
+    torch.cuda.empty_cache()
+    torch.backends.cudnn.benchmark = True
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # --------------------------------
+    # load model
+    # --------------------------------
+    model_path = os.path.join('MSRResNetx4_model', 'MSRResNetx4.pth')
+    model = MSRResNet(in_nc=3, out_nc=3, nf=64, nb=16, upscale=4)
+    model.load_state_dict(torch.load(model_path), strict=True)
+    model.eval()
+    for k, v in model.named_parameters():
+        v.requires_grad = False
+    model = model.to(device)
+
+    # number of parameters
+    number_parameters = sum(map(lambda x: x.numel(), model.parameters()))
+    logger.info('Params number: {}'.format(number_parameters))
+
+    # --------------------------------
+    # read image
+    # --------------------------------
+    L_folder = os.path.join(testsets, testset_L, 'X4')
+    E_folder = os.path.join(testsets, testset_L+'_results')
+    util.mkdir(E_folder)
+
+    # record PSNR, runtime
+    test_results = OrderedDict()
+    test_results['runtime'] = []
+
+    logger.info(L_folder)
+    logger.info(E_folder)
+    idx = 0
+
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    for img in util.get_image_paths(L_folder):
+
+        # --------------------------------
+        # (1) img_L
+        # --------------------------------
+        idx += 1
+        img_name, ext = os.path.splitext(os.path.basename(img))
+        logger.info('{:->4d}--> {:>10s}'.format(idx, img_name+ext))
+
+        img_L = util.imread_uint(img, n_channels=3)
+        img_L = util.uint2tensor4(img_L)
+        img_L = img_L.to(device)
+
+        start.record()
+        img_E = model(img_L)
+        end.record()
+        torch.cuda.synchronize()
+        test_results['runtime'].append(start.elapsed_time(end))  # milliseconds
+
+
+#        torch.cuda.synchronize()
+#        start = time.time()
+#        img_E = model(img_L)
+#        torch.cuda.synchronize()
+#        end = time.time()
+#        test_results['runtime'].append(end-start)  # seconds
+
+        # --------------------------------
+        # (2) img_E
+        # --------------------------------
+        img_E = util.tensor2uint(img_E)
+
+        # util.imsave(img_E, os.path.join(E_folder, img_name+ext))
+    ave_runtime = sum(test_results['runtime']) / len(test_results['runtime']) / 1000.0
+    logger.info('------> Average runtime of ({}) is : {:.6f} seconds'.format(L_folder, ave_runtime))
+
+if __name__ == '__main__':
+
+    main()
